0ac86e408a5f2fa5a93def061cb610e8546eb848
tolva <8457875+deild@users.noreply.github.com>
2020-12-18 20:05:23 +0100

    Fix sha256 castor_whispers

--- Formula/castor_whispers.rb
+++ Formula/castor_whispers.rb
@@ -2,7 +2,7 @@ class CastorWhispers < Formula
   desc "Utility to work with markdown text files"
   homepage "https://github.com/Jimskapt/castor_whispers"
   url "https://github.com/Jimskapt/castor_whispers/releases/download/3.2.0/castor_whispers_V3.2.0_macos_x86_64"
-  sha256 "8a57fb147cd75db4a304eab9c1e042891c71824adeacb853ba6ae475a0dc01df"
+  sha256 "ebac51c0e86c25898ecf0d1d1183d55465cdfec32dc4420f8404296c63f4768a"
   license ""
 
   def install
