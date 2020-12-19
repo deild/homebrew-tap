9cda7931f1101907d7505d54668dc4a7806518a6
tolva <8457875+deild@users.noreply.github.com>
2020-12-19 15:22:37 +0100

    fix dutree 0.2.16_1

--- Formula/dutree.rb
+++ Formula/dutree.rb
@@ -1,9 +1,10 @@
-class MdbookLinkcheck < Formula
+class Dutree < Formula
   desc "Tool to analyze file system usage written in Rust"
   homepage "https://github.com/nachoparker/dutree"
   url "https://github.com/deild/dutree/releases/download/v0.2.16/dutree-v0.2.16-x86_64-apple-darwin.tar.gz"
   sha256 "5583564ffb9543e07f6c96677b039a828950e6635e77d0019d58e4e50ec34950"
   version "0.2.16"
+  revision 1
   license "MIT"
 
   def install
