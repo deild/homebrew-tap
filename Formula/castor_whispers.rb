a1a8d14bb29d1f1da01d98a1a23f8d99d8f6b5c8
tolva <8457875+deild@users.noreply.github.com>
2020-12-19 14:02:08 +0100

    Fix version 3.2.0 castor_whispers

--- Formula/castor_whispers.rb
+++ Formula/castor_whispers.rb
@@ -3,6 +3,7 @@ class CastorWhispers < Formula
   homepage "https://github.com/Jimskapt/castor_whispers"
   url "https://github.com/Jimskapt/castor_whispers/releases/download/3.2.0/castor_whispers_V3.2.0_macos_x86_64"
   sha256 "ebac51c0e86c25898ecf0d1d1183d55465cdfec32dc4420f8404296c63f4768a"
+  version "3.2.0"
   revision 2
   license ""
 
