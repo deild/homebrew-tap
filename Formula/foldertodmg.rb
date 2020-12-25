c9e522103b911c5f63fb57936c10ddf89e5469de
tolva <8457875+deild@users.noreply.github.com>
2020-12-25 22:10:00 +0100

    foldertodmg 0.1.3

--- Formula/foldertodmg.rb
+++ Formula/foldertodmg.rb
@@ -1,8 +1,8 @@
 class Foldertodmg < Formula
   desc "foldertodmg is a personnal helper on hdiutil"
   homepage "https://github.com/deild/foldertodmg"
-  url "https://github.com/deild/foldertodmg/releases/download/v0.1.2/foldertodmg-macos-x86_64-v0.1.2.tar.gz"
-  sha256 "65606dfcc9af92d0fb4fa2acd8a8c3fa337b37a37b5c7d8a7aebc2168678a3bb"
+  url "https://github.com/deild/foldertodmg/releases/download/v0.1.3/foldertodmg-macos-x86_64-v0.1.3.tar.gz"
+  sha256 "c166c4f230a7d84078e08b206ec1dd0e87ea0adb8d16ac6c1fcb1735ce175a3e"
   #version "0.1.0"
   #revision 1
   license "MIT"
