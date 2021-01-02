175bf6a2a340577273b701ad7f3fc92dbab51bda
tolva <8457875+deild@users.noreply.github.com>
2021-01-02 17:58:43 +0100

    folder2dmg 0.1.5

--- Formula/folder2dmg.rb
+++ Formula/folder2dmg.rb
@@ -1,8 +1,8 @@
 class Folder2dmg < Formula
   desc "folder2dmg is a personnal helper on hdiutil"
   homepage "https://github.com/deild/folder2dmg"
-  url "https://github.com/deild/folder2dmg/releases/download/v0.1.4/folder2dmg-macos-v0.1.4.tar.gz"
-  sha256 "63d0543df4ae8ffe21ac3b13e55478207d8471a383f775fb4f47c2d6b46b18a0"
+  url "https://github.com/deild/folder2dmg/releases/download/v0.1.5/folder2dmg-macos-v0.1.5.tar.gz"
+  sha256 "fd68fdf6f44b1cc80e031e5feaa644ffcc0cd97a72b4ef186f729a22bec8ff00"
   #version "0.1.0"
   #revision 1
   license "MIT"
